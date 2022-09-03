b894a9c4974d8a6d06b03984092dba9a7626d461
tushuqi <72071329+TuShuqi@users.noreply.github.com>
2022-09-03 20:52:17 +0800

    Update sift.py

--- sift.py
+++ sift.py
@@ -1,19 +1,14 @@
 import numpy as np
-import cv2
 import os
 import cv2 as cv
 import matplotlib.pyplot as plt
 
 # 1) 以灰度图的形式读入图片
-
-# img_1 = cv.imread(r'gym1.jpg', cv.IMREAD_GRAYSCALE)
-# img_2 = cv.imread(r'gym2.jpg', cv.IMREAD_GRAYSCALE)
-
 img_1 = cv.imread(r'yifulou3.jpg', cv.IMREAD_GRAYSCALE)
 img_2 = cv.imread(r'yifulou2.jpg', cv.IMREAD_GRAYSCALE)
 
 # 2) SIFT特征计算
-sift = cv.xfeatures2d.SIFT_create()
+sift = cv.SIFT_create()
 
 img_kp1, img_des1 = sift.detectAndCompute(img_1, None)
 img_kp2, img_des2 = sift.detectAndCompute(img_2, None)
@@ -36,12 +31,7 @@ print(goodMatch[:20])
 
 img_out = cv.drawMatchesKnn(img_1, img_kp1, img_2, img_kp2, goodMatch[:15], None, flags=2)
 
-# plt.subplot(221), plt.imshow(img_1, 'gray'), plt.title('Original Image1')
-# plt.axis('off')
-# plt.subplot(222), plt.imshow(img_2, 'gray'), plt.title('Original Image2')
-# plt.axis('off')
 cv.imshow('image', img_out)           # 展示图片
-# plt.subplot(223), plt.imshow( img_out), plt.title('Original Image3')
-# plt.axis('off')
+
 cv.waitKey(0)                         # 等待按键按下
 cv.destroyAllWindows()                # 清除所有窗口
